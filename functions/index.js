6726b4675097d81756dc558b52a7e5146626c2ee
vineet kumar <vineet.kumar@dikonia.in>
2021-03-16 12:05:39 +0530

    new

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ exports.sendPush = functions.https.onRequest((request, result) => {
 const serverKey = "AAAAuvzuxTM:APA91bEFnoRRsFK6aCLUKGMFJjifPLAZX6yHmVXMjmzrFXbcBZeDMvN_13vaECsAHCCg8czuxdzOSN4hbgwUE4tPD9suv2pbOeeRoGwaz7Bg0JVrvdS10RIkaaxEEL7LcfHI0x4edcOoDF9kLkj-I0IN7sndOr5AOQ"; 
   const fcm = new FCM(serverKey);
   const message = { 
-to: "ccVQUJwMTV2KD7Xp43XCYJ:APA91bGyShpGvEO8yeB8tJUY2eNu5KT4gT6trTlHIIRaNHefQlxGTB58QnUPQ9wVHOeRyHBO6uBlVRSTzlY3eabjqI3sGMfGCHDdSX5aVgOT6UKI_L8c8kNuyIGLWTQ5iOv0ltkqxYgE",
+to: "f5Cth2zZT6uRWoesPXiGfw:APA91bG0NfsywAlHJFLtmuvHdBU9vQ_cEMbZS0LSFuPhoshm88yhfa1TsYxzG9BU9w7MsrmCBkOdsZU6N6Is3n3j5qEL0fZGXEVVU563v9210c10S4BYy3I1667TlAsULywYFlTkggxl",
 notification: {
 title: request.body.title, 
 body: request.body.body
